7cb623235d9790454534c0f5cf1918878bcb414f
Sudip Acharya <drxking456@gmail.com>
2024-11-06 22:20:07 +0545

    Message

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 const App = () => {
   return (
     <div className="relative min-h-screen overflow-x-hidden">
-      <div className="overlay bg-[url('./assets/beckground_04min.jpeg')] blur-sm bg-cover bg-center bg-no-repeat absolute -z-10 h-full w-full"></div>
+      <div className="overlay bg-[url('./assets/beckground_04min.jpeg')] blur-sm bg-cover bg-center bg-no-repeat fixed top-0 -z-10 h-full w-full"></div>
       <Navbar />
       <Hero />
     </div>
