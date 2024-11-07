9df9ef5e9527a1716b78be9fc7360dff32195866
Sudip Acharya <drxking456@gmail.com>
2024-11-07 23:22:25 +0545

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Zoomeffect from "./components/Zoomeffect";
 const App = () => {
   return (
     <div className="relative min-h-screen overflow-hidden">
-      <div className="overlay bg-[url('./assets/beckground_04min.jpeg')] blur-sm bg-cover bg-center bg-no-repeat fixed top-0 -z-10 h-full w-full"></div>
+      <div style={{filter:`hue-rotate(30deg)`,transform:`scale(2)`}} className=" origin-center overlay bg-[url('./assets/beckground_04min.jpeg')] blur-lg bg-cover bg-center bg-no-repeat fixed top-0 -z-10 h-full w-full"></div>
       <Navbar />
       <Hero />
       {/* <Zoomeffect /> */}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef, useEffect } from "react";
@@ -6,6 +6,8 @@ gsap.registerPlugin(useGSAP);
 const Hero = () => {
   let [rotX, setrotX] = useState(0);
   let [rotY, setrotY] = useState(0);
+
+  let star = useRef(null);
   let effect = useRef(null);
   useEffect(() => {
     let hoverer = effect.current;
@@ -21,20 +23,23 @@ const Hero = () => {
       setrotX(0);
       setrotY(0);
     });
-  }, []);
+    star.current.addEventListener("mousedown",()=>{
+      star.current.style.scale = 0.7
+    })
+    star.current.addEventListener("mouseup",()=>{
+      star.current.style.scale = 1
+    })
+    star.current.addEventListener("mouseleave",()=>{
+      star.current.style.scale = 1
+    })
+  });
 
   useGSAP(() => {
     gsap.to("#animate", {
-      transform: `rotateY(${rotX / 150}deg) rotateX(${-(
-        rotY / 100
+      transform: `rotateY(${rotX / 300}deg) rotateX(${-(
+        rotY / 300
       )}deg) translateY(${-(rotY / 20)}px) translateX(${-(rotX / 20)}px)`,
     });
-    gsap.to(".stars", {
-      transform: `rotate(-15deg) translateY(${-(rotY / 10)}px) translateX(${-(rotX / 10)}px)`,
-    });
-    // gsap.to(".stars-2", {
-    //   transform: `rotate(30deg) translateY(${(rotY / 20)}px) translateX(${(rotX / 20)}px)`,
-    // });
   }, [rotX, rotY]);
 
   return (
@@ -43,14 +48,14 @@ const Hero = () => {
       style={{ perspective: "1000px" }}
       className="hero-container flex items-center flex-col pt-20 px-20 relative"
     >
-      <div className="stars absolute top-[15%] rotate-[-15deg]  z-10 left-[20%]">
-        <img src="/star.png" className="h-40 w-40 " />
+      <div
+        ref={star}
+        className="stars duration-300   absolute top-[15%]   z-10 left-[20%]"
+      >
+        <img src="/star.png" className="h-40 w-40 pointer-events-none" />
       </div>
-      {/* <div className="stars-2 -z-10 absolute top-[-20%]  rotate-[30deg] blur-lg right-[10%]">
-        <img src="/star.png" className="h-[140vh] w-[100vh] " />
-      </div> */}
       <div id="animate">
-        <div className="text-[9vw] leading-[0.9] font-light  uppercase hero-text">
+        <div className="text-[9vw] leading-[0.9] font-light text-[rgba(0,0,0,0.95)]  uppercase hero-text">
           <p className="flex justify-between">
             <span>DESign</span> <span>that</span> <span></span>
             <span></span>
@@ -70,7 +75,7 @@ const Hero = () => {
       </div>
       <div className="arrow font-[poppins] uppercase font-light text-sm flex flex-col items-center">
         <p>Scroll</p>
-        <i class="ri-arrow-down-s-line text-xl animate-bounce mt-2"></i>
+        <i className="ri-arrow-down-s-line text-xl animate-bounce mt-2"></i>
       </div>
     </div>
   );
